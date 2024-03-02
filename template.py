000e7cbead4e7420862b17039514387e560a90af
mahendra867 <mahendramahesh2001@gmail>
2024-03-02 10:07:14 +0530

    updated the pkg

--- template.py
+++ template.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import logging
 
 package_name = "mongodb_connect"
 
@@ -31,4 +32,7 @@ for filepath in list_of_files:
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
-            pass # create an empty file
+            logging.info(f"creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} is already exists")
